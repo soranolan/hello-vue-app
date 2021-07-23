aa40d9632727eccc122d5ff235d7c52e69b7be69
Sorano Lan <soranolan@example.com>
2021-07-23 13:45:48 +0800

    stage

--- src/methods/F03001.js
+++ src/methods/F03001.js
@@ -2,10 +2,7 @@ import axios from "axios";
 import moment from 'moment';
 import { ElLoading, ElMessage } from 'element-plus';
 
-function handleCurrentChange(currentRow, oldCurrentRow) {
-  this.targetRow = (currentRow == null) ? "" : currentRow.id;
-}
-
+// validation block start =====================================================
 function validFindAllByUserId(findAllByUserIdFormRef) {
   this.$refs[findAllByUserIdFormRef].validate((valid, field) => {
     if (!valid) {
@@ -17,6 +14,30 @@ function validFindAllByUserId(findAllByUserIdFormRef) {
   });
 }
 
+function validfindOneByDocumentId(findOneByDocumentIdFormRef) {
+  this.$refs[findOneByDocumentIdFormRef].validate((valid, field) => {
+    if (!valid) {
+      this.findOneByDocumentIdFormVisible = true;
+      return false;
+    }
+    this.findOneByDocumentIdFormVisible = false;
+    this.findOneByDocumentId();
+  });
+}
+
+function validAddDocument(addDocumentFormRef) {
+  this.$refs[addDocumentFormRef].validate((valid, field) => {
+    if (!valid) {
+      this.addDocumentFormVisible = true;
+      return false;
+    }
+    this.addDocumentFormVisible = false;
+    ElMessage.success('Success');
+    this.addDocument();
+  });
+}
+// validation block end =======================================================
+// button block start =========================================================
 function findAllByUserId() {
   let superThis = this;
   let loadingInstance = ElLoading.service(this.ElLoadingConfig);
@@ -34,38 +55,6 @@ function findAllByUserId() {
       loadingInstance.close();
       ElMessage.error(superThis.$t('message.error'));
     });
-}
-
-function buildQueryString() {
-  let sortBy1 = this.findAllByUserIdFormModel.sortBy1;
-  let sortBy2 = this.findAllByUserIdFormModel.sortBy2;
-  let sortBy3 = this.findAllByUserIdFormModel.sortBy3;
-
-  let direct1 = this.findAllByUserIdFormModel.sortDirection1;
-  let direct2 = this.findAllByUserIdFormModel.sortDirection2;
-  let direct3 = this.findAllByUserIdFormModel.sortDirection3;
-  direct1 = (typeof direct1 === 'undefined') ? 'ASC' : direct1;
-  direct2 = (typeof direct2 === 'undefined') ? 'ASC' : direct2;
-  direct3 = (typeof direct3 === 'undefined') ? 'ASC' : direct3;
-
-  let sort1 = (typeof sortBy1 === 'undefined' || sortBy1.length == 0) ? '' : sortBy1 + ":" + direct1;
-  let sort2 = (typeof sortBy2 === 'undefined' || sortBy2.length == 0) ? '' : sortBy2 + ":" + direct2;
-  let sort3 = (typeof sortBy3 === 'undefined' || sortBy3.length == 0) ? '' : sortBy3 + ":" + direct3;
-
-  let ary = [sort1, sort2, sort3];
-  let queryString = ary.filter(str => str.length > 0).join(',');
-  return queryString;
-}
-
-function validfindOneByDocumentId(findOneByDocumentIdFormRef) {
-  this.$refs[findOneByDocumentIdFormRef].validate((valid, field) => {
-    if (!valid) {
-      this.findOneByDocumentIdFormVisible = true;
-      return false;
-    }
-    this.findOneByDocumentIdFormVisible = false;
-    this.findOneByDocumentId();
-  });
 }
 
 function findOneByDocumentId() {
@@ -86,6 +75,32 @@ function findOneByDocumentId() {
       console.log(error);
       ElMessage.error(superThis.$t('message.error'));
     });
+}
+
+function addDocument() {
+  
+}
+// button block end ===========================================================
+// helper block start =========================================================
+function buildQueryString() {
+  let sortBy1 = this.findAllByUserIdFormModel.sortBy1;
+  let sortBy2 = this.findAllByUserIdFormModel.sortBy2;
+  let sortBy3 = this.findAllByUserIdFormModel.sortBy3;
+
+  let direct1 = this.findAllByUserIdFormModel.sortDirection1;
+  let direct2 = this.findAllByUserIdFormModel.sortDirection2;
+  let direct3 = this.findAllByUserIdFormModel.sortDirection3;
+  direct1 = (typeof direct1 === 'undefined') ? 'ASC' : direct1;
+  direct2 = (typeof direct2 === 'undefined') ? 'ASC' : direct2;
+  direct3 = (typeof direct3 === 'undefined') ? 'ASC' : direct3;
+
+  let sort1 = (typeof sortBy1 === 'undefined' || sortBy1.length == 0) ? '' : sortBy1 + ":" + direct1;
+  let sort2 = (typeof sortBy2 === 'undefined' || sortBy2.length == 0) ? '' : sortBy2 + ":" + direct2;
+  let sort3 = (typeof sortBy3 === 'undefined' || sortBy3.length == 0) ? '' : sortBy3 + ":" + direct3;
+
+  let ary = [sort1, sort2, sort3];
+  let queryString = ary.filter(str => str.length > 0).join(',');
+  return queryString;
 }
 
 function dateFormat(row, column, cellValue, index) {
@@ -109,6 +124,11 @@ function setDisable(value, methodIndex) {
   target.methodIndex = methodIndex;
 }
 
+function handleCurrentChange(currentRow, oldCurrentRow) {
+  this.targetRow = (currentRow == null) ? "" : currentRow.id;
+}
+// helper block end ===========================================================
+// data block start ===========================================================
 function getApiMenuData() {
   return {
     targetRow: "",
@@ -126,6 +146,19 @@ function getApiMenuData() {
         { required: true, trigger: 'blur', message: this.$t('rules.required') },
         { max: 24, trigger: 'blur', message: this.$t('rules.max24') },
         { pattern: new RegExp(/^[a-zA-Z0-9]*$/), trigger: 'blur', message: this.$t('rules.alphanumeric') }
+      ],
+      creator: [
+        { required: true, trigger: 'blur', message: this.$t('rules.required') },
+        { max: 5, trigger: 'blur', message: this.$t('rules.max5') },
+        { pattern: new RegExp(/^[a-zA-Z0-9]*$/), trigger: 'blur', message: this.$t('rules.alphanumeric') }
+      ],
+      title:[
+        { required: true, trigger: 'blur', message: this.$t('rules.required') },
+        { max: 24, trigger: 'blur', message: this.$t('rules.max24') },
+      ],
+      content: [
+        { required: true, trigger: 'blur', message: this.$t('rules.required') },
+        { max: 120, trigger: 'blur', message: this.$t('rules.max120') },
       ]
     },
     findAllByUserIdFormVisible: false,
@@ -182,8 +215,23 @@ function getApiMenuData() {
     }
   };
 }
+// data block end =============================================================
 
 export default {
   name: 'F03001JS',
-  methods: { validFindAllByUserId, findAllByUserId, buildQueryString, validfindOneByDocumentId, findOneByDocumentId, dateFormat, setDisable, getApiMenuData, handleCurrentChange }
+  methods: {
+    validFindAllByUserId,
+    validfindOneByDocumentId,
+    validAddDocument,
+
+    findAllByUserId,
+    findOneByDocumentId,
+
+    buildQueryString,
+    dateFormat,
+    setDisable,
+
+    getApiMenuData,
+    handleCurrentChange
+  }
 };
