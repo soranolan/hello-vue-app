e96d62f8b02f9f74c0a49e153664732ca9f58151
Sorano Lan <soranolan@example.com>
2021-08-06 10:23:14 +0800

    remove bcryptjs.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,14 @@ import store from "./store";
 
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
-import axios from "axios";
+import axios from "./conf/axiosConfig";
 import VueAxios from "vue-axios";
 import moment from "moment";
 import i18n from "./i18n";
-import bcrypt from "bcryptjs";
-
-axios.defaults.baseURL = "http://localhost:8080/api/v1.0.0/todolist";
 
 createApp(App)
   .use(i18n)
   .use(router)
   .use(store)
-  .use(ElementPlus, axios, VueAxios, moment, bcrypt)
+  .use(ElementPlus, axios, VueAxios, moment)
   .mount("#app");
