7a626ccb78c108b95fea5cc4787ba3f8567330b2
Sorano Lan <soranolan@example.com>
2021-08-06 10:24:32 +0800

    add token.

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,14 @@ import { ref } from "vue";
 export default createStore({
   state: {
     activeUri: ref("/"),
+    token: "",
   },
   mutations: {
     setActiveUri(state, uri) {
       state.activeUri = ref(uri);
+    },
+    setToken(state, token) {
+      state.token = token;
     },
   },
   actions: {},
