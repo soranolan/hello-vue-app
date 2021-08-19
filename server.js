87355e42e2c9202cf62bcb1940f61a73ba165f48
Sorano Lan <soranolan@example.com>
2021-08-19 15:58:22 +0800

    try fix

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const serveStatic = require("serve-static");
-//const history = require("connect-history-api-fallback");
+const history = require("connect-history-api-fallback");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
+const middleware = serveStatic(path.join(__dirname, "dist"));
 
-app.use(serveStatic(path.join(__dirname, "dist")));
-//app.use(history());
-//app.use(serveStatic(path.join(__dirname, "/dist")));
-
-if (process.env.NODE_ENV === "production") {
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
-}
-
-//app.get("/", function (req, res) {
-//  res.render(path.join(__dirname + "/dist/index.html"));
-//});
+app.use(history());
+app.use(middleware);
 
 app.listen(port);
