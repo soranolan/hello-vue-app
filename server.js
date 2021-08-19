d5266a4d1de6fd92fd11f124970ed0125a03ffb7
Sorano Lan <soranolan@example.com>
2021-08-19 16:43:57 +0800

    try

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require("express");
-//const path = require("path");
-//const serveStatic = require("serve-static");
+const path = require("path");
+const serveStatic = require("serve-static");
 const history = require("connect-history-api-fallback");
-
-const app = express();
 const port = process.env.PORT || 3000;
 
+const app = express();
 app.use(
   history({
     verbose: true,
     disableDotRule: true,
   })
 );
-
-app.use(express.static(__dirname + "/dist"));
-
+app.use(serveStatic(path.join(__dirname, "dist"), { dotfiles: "allow" }));
 app.listen(port);
