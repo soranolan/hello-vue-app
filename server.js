f645fa894445ec901bf55daebf1c630b29f82e04
Sorano Lan <soranolan@example.com>
2021-08-19 14:27:38 +0800

    try fix vue history mode refresh 404
    without server side config.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 const path = require("path");
-const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+
 const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(
+  history({
+    verbose: true,
+    disableDotRule: true,
+  })
+);
 app.listen(port);
