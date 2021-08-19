e65fed1a7723a5c30702dffb95909fce6328adbc
Sorano Lan <soranolan@example.com>
2021-08-19 16:09:00 +0800

    try fix

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const serveStatic = require("serve-static");
 const history = require("connect-history-api-fallback");
@@ -11,3 +12,17 @@ app.use(history());
 app.use(middleware);
 
 app.listen(port);
+*/
+
+var history = require("connect-history-api-fallback");
+var express = require("express");
+var serveStatic = require("serve-static");
+
+const app = express();
+app.use(history());
+app.use(serveStatic(__dirname));
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+console.log("server started " + port);
