516f3c50a0cb6980bfbd8ad2f3449b76b3f38f68
Sorano Lan <soranolan@example.com>
2021-08-19 16:25:38 +0800

    try fix

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 const express = require("express");
 const serveStatic = require("serve-static");
 const history = require("connect-history-api-fallback");
@@ -6,23 +5,15 @@ const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
-const middleware = serveStatic(path.join(__dirname, "dist"));
 
-app.use(history());
-app.use(middleware);
+app.use(
+  history({
+    index: path.join(__dirname, "dist") + "/index.html",
+    verbose: true,
+    disableDotRule: true,
+  })
+);
+
+app.use(serveStatic(path.join(__dirname, "dist")));
 
 app.listen(port);
-*/
-
-var history = require("connect-history-api-fallback");
-var express = require("express");
-var serveStatic = require("serve-static");
-
-const app = express();
-app.use(history());
-app.use(serveStatic(__dirname));
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-
-console.log("server started " + port);
