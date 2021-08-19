a4cc248150b78366188f66902a51a38affb0a9ed
Sorano Lan <soranolan@example.com>
2021-08-19 16:47:44 +0800

    test

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
-const history = require("connect-history-api-fallback");
+//const history = require("connect-history-api-fallback");
 const port = process.env.PORT || 3000;
 
 const app = express();
+/*
 app.use(
   history({
     verbose: true,
     disableDotRule: true,
   })
 );
+*/
 app.use(serveStatic(path.join(__dirname, "dist"), { dotfiles: "allow" }));
 app.listen(port);
